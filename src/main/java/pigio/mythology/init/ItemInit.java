7d375b98f6e88a7bcbead4aea7cd7c954672aa14
Pigio1 <38838851+Pigio1@users.noreply.github.com>
2018-04-29 18:02:55 -0500

    addde main files when i made this

---
+++ src/main/java/pigio/mythology/init/ItemInit.java
@@ -0,0 +1,14 @@
+package pigio.mythology.init;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.minecraft.item.Item;
+import pigio.mythology.objects.items.ItemBase;
+
+public class ItemInit 
+{
+	public static final List<Item> ITEMS = new ArrayList<Item>();
+	
+	public static final Item BAETYLUS_INGOT = new ItemBase("ingot_baetylus");
+}
---
+++ src/main/java/pigio/mythology/util/Reference.java
@@ -0,0 +1,11 @@
+package pigio.mythology.util;
+
+public class Reference 
+{
+		public static final String MODID = "mm";
+		public static final String NAME = "Mythology Mod";
+		public static final String VERSION = "1.0";
+		public static final String CLIENT = "pigio.mythology.proxy.ClientProxy";
+		public static final String COMMON = "pigio.mythology.proxy.CommonProxy";
+
+}
